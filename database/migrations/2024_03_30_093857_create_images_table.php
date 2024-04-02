29277e5cb1d7cf3e7a15b3a814a91e496995b2a3
Colin-Kapsner <colinkapsner@yahoo.com>
2024-04-01 19:22:28 -0600

    pictures table -> images table

--- database/migrations/2024_03_30_093857_create_images_table.php
+++ database/migrations/2024_03_30_093857_create_images_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pictures', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->foreignId('movie_id')->constrained()->onDelete('cascade');
-            $table->string('img');
+            $table->string('image');
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pictures');
+        Schema::dropIfExists('images');
     }
 };
